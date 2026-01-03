17e5eb89cbbe0377e9f10fbc3f7d226fd82eb680
Haris Alicic <haris.alicic1@gmail.com>
2026-01-03 08:29:55 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const main = async () => {
+  try {
+    console.log('Starting the application...');
+  } catch (error) {
+    console.error('Error starting the application:', error);
+  }
+};
+
+main();
